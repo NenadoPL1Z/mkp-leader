3d26dbcc190125d75a0b192f9b1fd47eb01ec8a3
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-01-02 20:29:02 +0500

    feat: add logs

--- src/lib/http/index.ts
+++ src/lib/http/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { API_URL, TIMEOUT } from "@app/lib/constants";
+import { API_URL, IS_DEV, TIMEOUT } from "@app/lib/constants";
 import { jwt } from "@app/lib/modules/asyncStorage/tokens";
-import type { InternalAxiosRequestConfig } from "axios";
 
 const createInstance = () => {
   const config = axios.create({
@@ -9,17 +8,65 @@ const createInstance = () => {
     timeout: TIMEOUT,
   });
 
-  config.interceptors.request.use(async (request) => {
-    const { access_token } = await jwt.getAccessToken();
+  config.interceptors.request.use(
+    async (request) => {
+      const { access_token = "" } = await jwt.getAccessToken();
+      request.headers.Authorization = `Bearer ${access_token}`;
 
-    return {
-      ...request,
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        ...request.headers,
+      if (IS_DEV) {
+        console.log(
+          `🚀🚀🚀 Начало запрос: [${request.method?.toUpperCase()}] ${
+            request.url
+          }`,
+          {
+            request,
+            headers: request.headers,
+            data: request.data,
+          },
+        );
+      }
+
+      return request;
+    },
+    (error) => {
+      if (IS_DEV) console.error("⛔⛔⛔ Ошибка запроса:", error);
+      return Promise.reject(error);
+    },
+  );
+
+  if (IS_DEV) {
+    config.interceptors.response.use(
+      (response) => {
+        console.log(
+          `✅✅✅ Конец запроса: [${response.status}] ${response.config.url}`,
+          {
+            response,
+            headers: response.headers,
+            data: response.data,
+          },
+        );
+        return response;
       },
-    } as InternalAxiosRequestConfig;
-  });
+      (error) => {
+        if (error.response) {
+          console.error(
+            `⛔⛔⛔ Ошибка ответа: [${error.response.status}] ${error.config.url}`,
+            {
+              error,
+              response: error.response,
+              data: error.response.data,
+            },
+          );
+        } else {
+          console.error("🌐🌐🌐 Ошибка сети:", {
+            error,
+            message: error.message,
+          });
+        }
+        return Promise.reject(error);
+      },
+    );
+  }
 
   return config;
 };
