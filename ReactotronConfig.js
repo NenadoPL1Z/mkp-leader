630852d9fb9bf148e332b5909ec5a4203f8961d4
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-01-03 17:48:09 +0500

    fix: config

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,11 +2,12 @@ import Reactotron from "reactotron-react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 Reactotron.setAsyncStorageHandler(AsyncStorage)
-  .configure() // controls connection & communication settings
+  .configure()
   .useReactNative({
+    devTools: true,
     asyncStorage: true,
     networking: { ignoreUrls: /symbolicate/ },
-    editor: false, // there are more options to editor
-    overlay: false, // just turning off overlay
-  }) // add all built-in react native plugins
-  .connect(); // let's connect!
+    editor: false,
+    overlay: false,
+  })
+  .connect();
