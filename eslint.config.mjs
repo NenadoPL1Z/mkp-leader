254441a5b215a127563a2b449d0f9e074a5feb22
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-01-05 00:22:08 +0500

    feat: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,15 @@
 import globals from "globals";
 import javascript from "@eslint/js";
 import typescript from "typescript-eslint";
-import react from "eslint-plugin-react";
-import reactNative from "eslint-plugin-react-native";
-import reactHooks from "eslint-plugin-react-hooks";
-import prettierConfig from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier/recommended";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReactNative from "eslint-plugin-react-native";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
-const reactHookConfig = {
-  plugins: {
-    "react-hooks": reactHooks,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-  },
-};
-
-const reactNativeConfig = {
-  plugins: {
-    "react-native": reactNative,
-  },
-  rules: {
-    ...reactNative.configs.all.rules,
-    "react-native/sort-styles": "off",
-    "react-native/no-inline-styles": "warn",
-  },
-};
+const files = ["**/*.{js,ts,tsx}"];
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,ts,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   {
     ignores: [
@@ -40,14 +20,66 @@ export default [
       "android",
       "ios",
       "node_modules",
+      "babel.config.js",
+      "metro.config.js",
+      "react-native.config.js",
     ],
   },
   javascript.configs.recommended,
   ...typescript.configs.recommended,
-  react.configs.flat.recommended,
-  react.configs.flat["jsx-runtime"],
-  reactHookConfig,
-  reactNativeConfig,
-  prettierConfig,
-  prettierPlugin,
+  eslintPluginPrettier,
+  // eslintPluginReact.configs.flat.recommended,
+  // eslintPluginReact.configs.flat["jsx-runtime"],
+  {
+    files,
+    rules: {
+      ...javascript.configs.recommended.rules,
+      "no-template-curly-in-string": "error",
+      "default-case-last": "error",
+      "default-param-last": "error",
+      "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+      "max-params": ["warn", 4],
+      "no-console": "error",
+      "no-empty": "error",
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+  {
+    files,
+    rules: {
+      "no-extra-boolean-cast": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      "react-native": eslintPluginReactNative,
+    },
+    rules: {
+      ...eslintPluginReactNative.configs.all.rules,
+      "react-native/no-raw-text": "off",
+      "react-native/sort-styles": "off",
+      "react-native/no-unused-styles": "error",
+      "react-native/no-inline-styles": "error",
+      "react-native/no-color-literals": "warn",
+      "react-native/no-single-element-style-arrays": "error",
+    },
+  },
 ];
